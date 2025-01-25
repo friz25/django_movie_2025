806a026c27da8cc88266c119dbe5be651a8b41a3
friz25 <friz25@gmail.com>
2025-01-25 23:36:00 +0200

    6.2 наШаблонили

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = MEDIA_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
