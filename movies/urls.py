7b07b9e75754bf62ddd0014b1aabab8d991cd40a
friz25 <friz25@gmail.com>
2025-01-27 09:58:03 +0200

    7.1 работает movie_detail.html

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.MoviewView.as_view())
+    path("", views.MoviewView.as_view()),
+    path("<int:pk>/", views.MovieDetailView.as_view()),
+    # по сути 'pk' это id запрашиваемого фильма
 ]
--- movies/views.py
+++ movies/views.py
@@ -3,9 +3,16 @@ from django.views.generic.base import View
 
 from .models import Movie
 
+
 class MoviewView(View):
     """Список фильмов"""
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, "movies/movies.html", {"movie_list": movies})
 
+
+class MovieDetailView(View):
+    """Полное описание фильма"""
+    def get(self, request, pk):
+        movie = Movie.objects.get(id=pk)
+        return render(request, 'movies/movie_detail.html', {"movie": movie})
