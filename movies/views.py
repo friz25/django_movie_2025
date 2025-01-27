bf17627b2efdc4ed07bc79f37f5f85408cffa317
friz25 <friz25@gmail.com>
2025-01-27 11:43:46 +0200

    7.3 ListView / DetailView

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render
+from django.views.generic import DetailView, ListView
 from django.views.generic.base import View
 
 from .models import Movie
 
+# Старый вид/класс / записан через View()
+# class MoviesView(View):
+#     """Список фильмов"""
+#     def get(self, request):
+#         movies = Movie.objects.all()
+#         return render(request, "movies/movies.html", {"movie_list": movies})
 
-class MoviewView(View):
+class MoviesView(ListView):
     """Список фильмов"""
-    def get(self, request):
-        movies = Movie.objects.all()
-        return render(request, "movies/movies.html", {"movie_list": movies})
+    model = Movie
+    queryset = Movie.objects.filter(draft=False) #вывести все кроме "черновиков"
+    # template_name = "movies/movies.html"
 
 
-class MovieDetailView(View):
+# class MovieDetailView(View):
+#     """Полное описание фильма"""
+#     def get(self, request, slug):
+#         movie = Movie.objects.get(url=slug)
+#         return render(request, 'movies/movie_detail.html', {"movie": movie})
+
+class MovieDetailView(DetailView):
     """Полное описание фильма"""
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movies/movie_detail.html', {"movie": movie})
+    model = Movie
+    slug_field = 'url'
