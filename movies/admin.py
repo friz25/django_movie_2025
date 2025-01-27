bea712aeb2d01c196750cb85c0faaf42077260a8
friz25 <friz25@gmail.com>
2025-01-27 19:31:59 +0200

    11 настройка Админки / кастомизация

--- movies/admin.py
+++ movies/admin.py
@@ -3,12 +3,59 @@ from .models import Category, Genre, Movie, \
     MovieShots, Rating, RatingStar, Reviews, Actor
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name",)  # будет ссылкой (открытия записи)
+
+
+class ReviewInLine(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")  # ток чтение / нельзя изменить (из админки)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'draft')
+    list_filter = ('category', 'year')  # фильтр панеь (справа)
+    search_fields = ('title', 'category__name')  # строка поиска
+    inlines = [ReviewInLine]
+    save_on_top = True  # копия меню "сохранинь" (сверху)
+    save_as = True  # можно создать новый фильм "редактируя прошлый"
+    list_editable = ("draft",)  # из списка/каталога менять прям
+    # fields = (('actors', 'directors', 'genres'), )
+    fieldsets = (
+        (None, {
+            "fields": (('title', 'tagline'),)
+        }),
+        (None, {
+            "fields": (('description', 'poster'),)
+        }),
+        (None, {
+            "fields": (('year', 'world_premiere', 'country'),)
+        }),
+        ("Actors", {
+            "classes": ("collapse",),
+            "fields": (('actors', 'directors', 'genres', 'category'),)
+        }),
+        (None, {
+            "fields": (('budget', 'fees_in_usa', 'fees_in_world'),)
+        }),
+        ("Options", {
+            "fields": (('url', 'draft'),)
+        }),
+    )  # чтоб поля в одну строку
+
+
+@admin.register(Reviews)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("name", 'email', 'parent', 'movie', 'id')
+    readonly_fields = ("name", "email")  # ток чтение / нельзя изменить (из админки)
+
+
 admin.site.register(Genre)
-admin.site.register(Movie)
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
-
