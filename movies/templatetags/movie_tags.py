ea939722cc3c03df73393d9ad30a66000138afca
friz25 <friz25@gmail.com>
2025-01-29 15:52:03 +0200

    15.2 Вывод последних добавленных фильмов / InclusionTag

--- movies/templatetags/movie_tags.py
+++ movies/templatetags/movie_tags.py
@@ -1,8 +1,15 @@
 from django import template
-from movies.models import Category
+from movies.models import Category, Movie
 
 register = template.Library() # для регистрации наших тегов (в django)
 
 @register.simple_tag() # этот декоратор позволит зарегать наш тег (в django)
 def get_categories():
+    """Вывод всех категорий"""
     return Category.objects.all()
+
+@register.inclusion_tag('movies/tags/last_movie.html')
+def get_last_movies(count=5):
+    """Возвращает последние 5 добавл фильмов"""
+    movies = Movie.objects.order_by("id")[:count]
+    return {'last_movies': movies}
